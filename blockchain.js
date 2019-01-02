cb4dc7681996ab953d74c6df7aed88ac6157224e
DevSoopark <sparkm0128@gmail.com>
2019-01-02 17:26:20 +0900

    add replace the chain

--- blockchain.js
+++ blockchain.js
@@ -14,6 +14,7 @@ class Blockchain {
 
   isValidChain(chain) {
     if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) return false;
+    
     for (let i=1; i<chain.length; i++) {
       const block = chain[i];
       const lastBlock = chain[i-1];
@@ -26,6 +27,20 @@ class Blockchain {
     }
   
     return true;
+  }
+
+  replaceChain(newChain) {
+    if (newChain.length <= this.chain.length) {
+      console.log('Received chain is not longer than the current chain.');
+      return;
+    } 
+    else if (!this.isValidChain(newChain)) {
+      console.log('The received chain is not valid.');
+      return;
+    }
+  
+    console.log('Replacing blockchain with the new chain.');
+    this.chain = newChain;
   }
 
 }
