6fa6226be835e937676864784a8c34a7c6c538a5
DevSoopark <sparkm0128@gmail.com>
2019-01-02 16:35:28 +0900

    add chain validation

--- blockchain.js
+++ blockchain.js
@@ -8,8 +8,26 @@ class Blockchain {
   addBlock(data) {
     const block = Block.mineBlock(this.chain[this.chain.length-1], data);
     this.chain.push(block);
+  
     return block;
   }
+
+  isValidChain(chain) {
+    if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) return false;
+    for (let i=1; i<chain.length; i++) {
+      const block = chain[i];
+      const lastBlock = chain[i-1];
+      if (
+        block.lastHash !== lastBlock.hash ||
+        block.hash !== Block.blockHash(block)
+      ) {
+        return false;
+      }
+    }
+  
+    return true;
+  }
+
 }
 
 module.exports = Blockchain;
