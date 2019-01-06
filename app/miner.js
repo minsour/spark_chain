1929d026de3df3ccafba1bce0de570d9d34dd162
DevSoopark <sparkm0128@gmail.com>
2019-01-07 00:39:32 +0900

    Add reward, clear and broadcast transactions

--- app/miner.js
+++ app/miner.js
@@ -1,3 +1,6 @@
+const Wallet = require('../wallet');
+const Transaction = require('../wallet/transaction');
+
 class Miner {
     constructor(blockchain, transactionPool, wallet, p2pServer) {
       this.blockchain = blockchain;
@@ -8,11 +11,15 @@ class Miner {
   
     mine() {
       const validTransactions = this.transactionPool.validTransactions();
-      // include a reward transaction for the miner
-      // create a block consisting of the valid transactions
-      // synchronize chains in the peer-to-peer server
-      // clear the transaction pool
-      // broadcast to every miner to clear their transaction pools
+      validTransaction.push(
+          Transaction.rewardTransaction(this.wallet, Wallet.blockchainWallet())
+      );
+      const block = this.blockchain.addBlock(validTransactions);
+      this.p2pServer.syncChains();
+      this.transactionPool.clear();
+      this.p2pServer.broadcastClearTransactions();
+
+      return block;
     }
   }
   
--- app/p2p_server.js
+++ app/p2p_server.js
@@ -3,7 +3,8 @@ const P2P_PORT = process.env.P2P_PORT || 5001;
 const peers = process.env.PEERS ? process.env.PEERS.split(',') : [];
 const MESSAGE_TYPES = {
   chain: 'CHAIN',
-  transaction: 'TRANSACTION'
+  transaction: 'TRANSACTION',
+  clear_transactions: 'CLEAR_TRANSACTIONS'
 };
 
 class P2pServer {
@@ -49,8 +50,11 @@ class P2pServer {
             case MESSAGE_TYPES.transaction:
               this.transactionPool.updateOrAddTransaction(data.transaction);
               break;
+            case MESSAGE_TYPES.clear_transactions:
+              this.transactionPool.clear();
+              break;
         }
-        
+
         console.log('data', data);
     });
   }
@@ -77,6 +81,12 @@ class P2pServer {
 
   broadcastTransaction(transaction) {
     this.sockets.forEach(socket => this.sendTransaction(socket, transaction));
+  }
+
+  broadcastClearTransaction() {
+      this.sockets.forEach(socket => socket.send(JSON.stringify({
+          type: MESSAGE_TYPES.clear_transactions
+      })));
   }
 }
 
