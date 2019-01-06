867024ff54d1303019f479f2ce7ee32abf4258f6
DevSoopark <sparkm0128@gmail.com>
2019-01-06 17:04:43 +0900

    Add transactions_with_wallet

--- app/index.js
+++ app/index.js
@@ -2,11 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const Blockchain = require('../blockchain');
 const P2pServer = require('./p2p_server');
+const Wallet = require('../wallet');
+const TransactionPool = require('../wallet/transaction_pool');
+
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
 const bc = new Blockchain();
 const p2pServer = new P2pServer(bc);
+const wallet = new Wallet();
+const tp = new TransactionPool();
 
 app.use(bodyParser.json());
 
@@ -21,6 +26,16 @@ app.post('/mine', (req, res) => {
     p2pServer.syncChains();
 
     res.redirect('/blocks');
+});
+
+app.get('/transactions', (req, res) => {
+    res.json(tp.transactions);
+  });
+  
+app.post('/transact', (req, res) => {
+  const { recipient, amount } = req.body;
+  const transaction = wallet.createTransaction(recipient, amount, tp);
+  res.redirect('/transactions');
 });
 
 app.listen(HTTP_PORT, () => console.log(`Listening on port: ${HTTP_PORT}`));
